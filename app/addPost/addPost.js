7f5e9ea9e09a0a88cc6dae16290376dfea6a0fbf
Eddie Massey III <eem33@case.edu>
2018-02-21 13:16:34 -0500

    quick update for kids

--- app/addPost/addPost.js
+++ app/addPost/addPost.js
@@ -4,35 +4,46 @@ angular.module('myApp.addPost', ['ngRoute', 'firebase'])
 
     .config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/addPost', {
-	    templateUrl: 'addPost/addPost.html',
-	    controller: 'AddPostCtrl'
+	    templateUrl:'addPost/addPost.html',
+	    controller:'AddPostCtrl'
 	});
+
     }])
 
-    .controller('AddPostCtrl', ['$scope', '$location','$firebaseAuth', function($scope,$location,$firebaseAuth) {
+.controller('AddPostCtrl', ['$scope', '$firebase', function($scope, $location, $firebase) {
 
-	$scope.SignIn = function(e) {
-	    e.preventDefault();
-	    console.log("In sign in");
-	    var email = $scope.user.email;
-	    var password = $scope.user.password;
+    $scope.AddPost = function() {
+	
+	var title = $scope.post.title;
+	var image = $scope.post.image;
+	var summary = $scope.post.summary;
+	var content = $scope.post.content;
 
-
-	    firebase.auth().signInWithEmailAndPassword(email,password)
-	    
-		.then(function(user) {
-		    console.log("Authentication successful");
-		    $location.path('/home');
-		}, function(error) {
-
-		    var errorCode = error.code;
-		    var errorMessage = error.message;
-		    console.log(errorMessage, errorCode);
-		});
+	var postData = {
+	    title: title,
+	    image: "not/yet.jpg",
+	    summary: summary,
+	    content: content	    
 	}
-    }]);
+	
+	// Get a key for a new Post.
+	var newPostKey = firebase.database().ref().child('posts').push().key;
 
-	    
+	// Write the new post's data simultaneously in the posts list and the user's post list.
+	var updates = {};
+	updates['/posts/' + newPostKey] = postData;
 
 
+	firebase.database().ref().update(updates)
+	
+	    .then(function(){
+		console.log("Post updated")
+	    })
+	
+	    .catch(function(error) {
+		console.log("Error in update", error.message);
+	    });
+    	
+    }
 
+}]);
--- app/home/home.js
+++ app/home/home.js
@@ -13,10 +13,45 @@ angular.module('myApp.home', ['ngRoute','firebase'])
 
     $scope.posts = [
 	{
-	    title: "God loves us",
+	    title: "Lesson 1: Change Board Size",
 	    image: "img/psd-4.jpg",
 	    summary: "This is the sum of the matter",
+	    link:"https://s3.amazonaws.com/mg-curriculum-beta/1-1-board-size.mp4"
+	},
+	{
+	    title: "Lesson 2: Drawing Ellipses, Ovals, and Circles",
+	    image: "img/psd-4.jpg",
+	    summary: "This is the sum of the matter",
+	    link: "https://s3.amazonaws.com/mg-curriculum-beta/1-2-circles.mp4"
+	},
+	{
+	    title: "Lesson 3: Snowman Solution",
+	    image: "img/psd-4.jpg",
+	    summary: "This is the sum of the matter",
+	    link:"https://s3.amazonaws.com/mg-curriculum-beta/1-3-challenge-solution-snowman-Challenge-Intro.mp4"
+	},
+
+	{
+	    title: "Lesson 4: Snowman Solution",
+	    image: "img/psd-4.jpg",
+	    summary: "This is the sum of the matter",
+	    link:"https://s3.amazonaws.com/mg-curriculum-beta/1.4-Circle-Challenge-Solution.mp4"
+	},
+	{
+	    title: "Lesson 5: Drawing Rectangles",
+	    image: "img/psd-4.jpg",
+	    summary: "This is the sum of the matter",
+	    link:"https://s3.amazonaws.com/mg-curriculum-beta/1-5-rectangles.mp4"
+	},
+		{
+	    title: "Lesson 6: Movie Scene Project",
+	    image: "img/psd-4.jpg",
+	    summary: "This is the sum of the matter",
+	    link:"https://s3.amazonaws.com/mg-curriculum-beta/1.6-Documentation-and-Project-Challenge.mp4"
 	}
+
+	
+
     ];
 
 }]);
